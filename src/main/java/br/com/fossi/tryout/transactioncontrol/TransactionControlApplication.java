11559d8eb5be872f0534636f2e52308f878dc280
Fernando Avila Fossi Silveira <fasilveira@banestes.com.br>
2022-08-28 09:22:19 -0300

    Configuração Inicial do Projeto

---
+++ src/main/java/br/com/fossi/tryout/transactioncontrol/TransactionControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.fossi.tryout.transactioncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionControlApplication.class, args);
+	}
+
+}
